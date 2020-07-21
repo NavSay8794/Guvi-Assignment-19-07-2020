6796b58c27f7f028747c6f8ec7cf8228a14b8895
Naveed Sayyed <sayyednaveed107@gmail.com>
2020-07-21 13:08:40 +0530

    Callback Hell Task

---
+++ callback.js
@@ -0,0 +1,41 @@
+var h1 = document.getElementById('change')
+console.log(h1)
+
+function countdown(){
+    setTimeout(function(){
+        h1.innerText = '10...'
+        setTimeout(function(){
+            h1.innerText = '9...'
+            setTimeout(function(){
+                h1.innerText = '8...'
+                setTimeout(function(){
+                    h1.innerText = '7...'
+                    setTimeout(function(){
+                        h1.innerText = '6...'
+                        setTimeout(function(){
+                            h1.innerText = '5...'
+                            setTimeout(function(){
+                                h1.innerText = '4...'
+                                setTimeout(function(){
+                                    h1.innerText = '3...'
+                                    setTimeout(function(){
+                                        h1.innerText = '2...'
+                                        setTimeout(function(){
+                                            h1.innerText = '1...'
+                                            setTimeout(function(){
+                                                h1.innerText = 'Bang Bang...'
+                                                
+                                            },1000)
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+}
+
+countdown()
